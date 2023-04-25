c841b3d083c3f19d2fea094cac14995941595968
birosbeni <Beni>
2023-04-25 10:48:09 +0200

    Update windows forms app

--- PirosBiros/PirosBiros/Form1.cs
+++ PirosBiros/PirosBiros/Form1.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 using Hotcakes.CommerceDTO.v1;
 using Hotcakes.CommerceDTO.v1.Catalog;
 using Hotcakes.CommerceDTO.v1.Client;
+using Hotcakes.CommerceDTO.v1.Orders;
+using Newtonsoft.Json;
 
 namespace PirosBiros
 {
@@ -16,7 +19,37 @@ namespace PirosBiros
 
         private void button1_Click(object sender, EventArgs e)
         {
-            apiHivas();
+            GetOrders();
+        }
+
+
+        public void GetOrders()
+        {
+            string url = "http://20.234.113.211:8084/";
+            string key = "1-bc670955-f477-441d-8f8c-60cd5d958f8e";
+
+            Api proxy = new Api(url, key);
+
+            // call the API to find all orders in the store
+            ApiResponse<List<CategorySnapshotDTO>> response = proxy.CategoriesFindAll();
+            string json = JsonConvert.SerializeObject(response);
+
+            ApiResponse<List<CategorySnapshotDTO>> deserializedResponse = JsonConvert.DeserializeObject<ApiResponse<List<CategorySnapshotDTO>>>(json);
+
+            MessageBox.Show(deserializedResponse.Content.ToString());
+
+            //DataTable dt = new DataTable();
+            //dt.Columns.Add("Id", typeof(int));
+            //dt.Columns.Add("bvin", typeof(string));
+            //dt.Columns.Add("FirstName", typeof(string));
+            //dt.Columns.Add("StoreId", typeof(long));
+
+            //foreach (OrderSnapshotDTO item in deserializedResponse.Content)
+            //{
+            //    dt.Rows.Add(item.Id, item.bvin, item.BillingAddress.FirstName, item.StoreId);
+            //}
+
+            //ordersDataGridView.DataSource = dt;
         }
 
         private static void apiHivas()
@@ -38,11 +71,13 @@ namespace PirosBiros
             //if (url == string.Empty) url = "http://20.234.113.211:8084/";
             //if (key == string.Empty) key = "1-bc670955-f477-441d-8f8c-60cd5d958f8e";
 
-            var proxy = new Api(url, key);
+            Api proxy = new Api(url, key);
+            ApiResponse<List<CategorySnapshotDTO>> response = proxy.CategoriesFindAll();
+
 
             var snaps = proxy.ProductOptionsFindAll();
 
-            MessageBox.Show(snaps.ToString());
+            MessageBox.Show(response.Content.ToString());
 
             if (snaps.Content != null)
             {
