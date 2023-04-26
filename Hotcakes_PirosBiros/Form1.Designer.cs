e52cf50ae1cebc4cdf98300d424e4712ab8b2829
Gergő Gombár <gombar.gergo@outlook.hu>
2023-04-26 16:52:06 +0200

    Creating Hotcakes WinForms

---
+++ Hotcakes_PirosBiros/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Hotcakes_PirosBiros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.ordersDataGridView = new System.Windows.Forms.DataGridView();
+            this.createOrderButton = new System.Windows.Forms.Button();
+            this.deleteOrderButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ordersDataGridView
+            // 
+            this.ordersDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ordersDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.ordersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.ordersDataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.ordersDataGridView.Location = new System.Drawing.Point(9, 10);
+            this.ordersDataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ordersDataGridView.Name = "ordersDataGridView";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ordersDataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.ordersDataGridView.RowHeadersWidth = 51;
+            this.ordersDataGridView.RowTemplate.Height = 24;
+            this.ordersDataGridView.Size = new System.Drawing.Size(817, 381);
+            this.ordersDataGridView.TabIndex = 0;
+            // 
+            // createOrderButton
+            // 
+            this.createOrderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.createOrderButton.Location = new System.Drawing.Point(9, 404);
+            this.createOrderButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.createOrderButton.Name = "createOrderButton";
+            this.createOrderButton.Size = new System.Drawing.Size(130, 34);
+            this.createOrderButton.TabIndex = 1;
+            this.createOrderButton.Text = "Create";
+            this.createOrderButton.UseVisualStyleBackColor = true;
+            this.createOrderButton.Click += new System.EventHandler(this.createOrderButton_Click);
+            // 
+            // deleteOrderButton
+            // 
+            this.deleteOrderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.deleteOrderButton.Location = new System.Drawing.Point(154, 404);
+            this.deleteOrderButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.deleteOrderButton.Name = "deleteOrderButton";
+            this.deleteOrderButton.Size = new System.Drawing.Size(130, 34);
+            this.deleteOrderButton.TabIndex = 2;
+            this.deleteOrderButton.Text = "Delete";
+            this.deleteOrderButton.UseVisualStyleBackColor = true;
+            this.deleteOrderButton.Click += new System.EventHandler(this.deleteOrderButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 523);
+            this.Controls.Add(this.deleteOrderButton);
+            this.Controls.Add(this.createOrderButton);
+            this.Controls.Add(this.ordersDataGridView);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ordersDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ordersDataGridView;
+        private System.Windows.Forms.Button createOrderButton;
+        private System.Windows.Forms.Button deleteOrderButton;
+    }
+}
+
---
+++ Hotcakes_PirosBiros/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Catalog;
+using Hotcakes.CommerceDTO.v1.Client;
+using Hotcakes.CommerceDTO.v1.Contacts;
+using Hotcakes.CommerceDTO.v1.Orders;
+using Newtonsoft.Json;
+
+namespace Hotcakes_PirosBiros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetOrders();
+        }
+
+        private void createOrderButton_Click(object sender, EventArgs e)
+        {
+            string url = "http://www.dnndev.me";
+            string key = "1-69c91ed8-fe63-40aa-8a5c-85be6757e079";
+
+            Api proxy = new Api(url, key);
+
+            // create a new order object
+            var order = new OrderDTO();
+
+            // add billing information
+            order.BillingAddress = new AddressDTO
+            {
+                AddressType = AddressTypesDTO.Billing,
+                City = "West Palm Beach",
+                CountryBvin = "BF7389A2-9B21-4D33-B276-23C9C18EA0C0",
+                FirstName = "John",
+                LastName = "Dough",
+                Line1 = "319 N. Clematis Street",
+                Line2 = "Suite 600",
+                Phone = "561-228-5319",
+                PostalCode = "33401",
+                RegionBvin = "7EBE4F07-A844-47B8-BDA8-863DDDF5C778"
+            };
+
+            // add at least one line item
+            order.Items = new List<LineItemDTO>();
+            order.Items.Add(new LineItemDTO
+            {
+                ProductId = "dfcae0ee-8bcf-4321-8b31-7883b5434285",
+                Quantity = 1
+            });
+
+            // add the shipping address
+            order.ShippingAddress = new AddressDTO();
+            order.ShippingAddress = order.BillingAddress;
+            order.ShippingAddress.AddressType = AddressTypesDTO.Shipping;
+
+            // specify who is creating the order
+            order.UserEmail = "info@hotcakescommerce.com";
+            order.UserID = "1";
+
+            // call the API to create the order
+            ApiResponse<OrderDTO> response = proxy.OrdersCreate(order);
+
+            GetOrders();
+        }
+
+        private void deleteOrderButton_Click(object sender, EventArgs e)
+        {
+            string url = "http://www.dnndev.me";
+            string key = "1-69c91ed8-fe63-40aa-8a5c-85be6757e079";
+
+            Api proxy = new Api(url, key);
+
+            // specify the order to delete
+            int rowIndex = ordersDataGridView.CurrentCell.RowIndex;
+            int columnIndex = ordersDataGridView.CurrentCell.ColumnIndex;
+
+            var orderId = ordersDataGridView[columnIndex, rowIndex].Value.ToString();
+
+            // call the API to delete the order
+            ApiResponse<bool> response = proxy.OrdersDelete(orderId);
+
+            GetOrders();
+        }
+
+        public void GetOrders()
+        {
+            string url = "http://20.234.113.211:8084/";
+            string key = "1-69c91ed8-fe63-40aa-8a5c-85be6757e079";
+
+            Api proxy = new Api(url, key);
+
+            // call the API to find all orders in the store
+            ApiResponse<List<CategorySnapshotDTO>> response = proxy.CategoriesFindAll();
+            string json = JsonConvert.SerializeObject(response);
+
+            ApiResponse<List<CategorySnapshotDTO>> deserializedResponse = JsonConvert.DeserializeObject<ApiResponse<List<CategorySnapshotDTO>>>(json);
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Name", typeof(string));
+            //dt.Columns.Add("bvin", typeof(string));
+            //dt.Columns.Add("FirstName", typeof(string));
+            //dt.Columns.Add("StoreId", typeof(long));
+
+            foreach (CategorySnapshotDTO item in deserializedResponse.Content)
+            {
+                dt.Rows.Add(item.Name);
+            }
+
+            ordersDataGridView.DataSource = dt;
+        }
+    }
+}
