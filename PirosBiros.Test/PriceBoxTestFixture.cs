189e35c9e604083a70da0a2b44866e492521a841
Márk Csákvári <mark.csakvari@stud.uni-corvinus.hu>
2023-04-30 13:06:17 +0200

    Unit Tesztelés megkezdése - Solution hozzáadása, 3A meghatározásának megkezdése.

---
+++ PirosBiros.Test/PriceBoxTestFixture.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirosBiros.Test
+{
+    public class PriceBoxTestFixture
+    {
+        [Test]
+        public void TestValidatePriceBox(string price, bool expectedResult)
+        {
+            //Arrange
+            //var x = 
+
+            //Act
+            //var acutalResult = 
+
+            //Assert
+
+        }
+    }
+}
---
+++ PirosBiros.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PirosBiros.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PirosBiros.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c08cd83e-25f8-4eb9-9084-4eaad593f396")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
